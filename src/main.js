5a5299a6b5aa8222e6fa104190a35d6211f041d7
Allen <allenlee599@gmail.com>
2025-03-15 17:34:09 +0800

    [Add] add router and views folder for this project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 // import Swiper from 'swiper';
 // import 'swiper/css';
 
-// const app = createApp(App);
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
 // app.use(Swiper);
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<!-- src/components/Home.vue -->
+<template>
+    <div>
+      <ImageSwiper />
+    </div>
+  </template>
+  
+  <script>
+  import ImageSwiper from '../components/ImageSwiper.vue';
+  export default {
+    name: 'HomeView',
+    components: {
+      ImageSwiper
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
